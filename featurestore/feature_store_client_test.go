ce33a0debeb205037aaf16e168c9097e05ae5841
bruceding <bruceding.jing@alibaba-inc.com>
2024-09-03 12:05:45 +0800

    Add seq feature testcase

--- featurestore/feature_store_client_test.go
+++ featurestore/feature_store_client_test.go
@@ -7,10 +7,13 @@ import (
 )
 
 func createFeatureSotreClient() (*FeatureStoreClient, error) {
-	accessId := os.Getenv("AccessId")
-	accessKey := os.Getenv("AccessKey")
+	accessId := os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_ID")
+	accessKey := os.Getenv("ALIBABA_CLOUD_ACCESS_KEY_SECRET")
+	fdbUser := os.Getenv("FEATUREDB_USERNAME")
+	fdbPassword := os.Getenv("FEATUREDB_PASSWORD")
 
-	return NewFeatureStoreClient("cn-beijing", accessId, accessKey, "fs_test_ots", WithDomain("paifeaturestore.cn-beijing.aliyuncs.com"))
+	return NewFeatureStoreClient("cn-beijing", accessId, accessKey, "fs_demo_featuredb", WithDomain("paifeaturestore.cn-beijing.aliyuncs.com"),
+		WithTestMode(), WithFeatureDBLogin(fdbUser, fdbPassword))
 }
 
 func TestGetFeatureViewOnlineFeatures(t *testing.T) {
@@ -65,6 +68,36 @@ func TestGetModelFeatureOnlineFeatures(t *testing.T) {
 
 	// get online features
 	features, err := model_feature.GetOnlineFeaturesWithEntity(map[string][]interface{}{"user_id": {"100000676", "100004208"}}, "user")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(features)
+}
+
+func TestGetSeqFeatureViewOnlineFeatures(t *testing.T) {
+
+	// init client
+	client, err := createFeatureSotreClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// get project by name
+	project, err := client.GetProject("fs_demo_featuredb")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// get featureview by name
+	seq_feature_view := project.GetFeatureView("seq_feature_test")
+	if seq_feature_view == nil {
+		t.Fatal("feature view not exist")
+	}
+
+	// get online features
+	features, err := seq_feature_view.GetOnlineFeatures([]interface{}{"199636459"}, []string{"*"}, nil)
 
 	if err != nil {
 		t.Error(err)
