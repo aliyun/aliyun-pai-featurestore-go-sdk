aa16bf3178beedf5ee1e095cc554ed7529e7027e
千枫 <qianfeng.syf@alibaba-inc.com>
2026-02-24 14:30:38 +0800

    feat: add parent-child entity support for GetOnlineFeaturesWithAggregatedSequence

--- domain/model.go
+++ domain/model.go
@@ -422,6 +422,8 @@ func (m *Model) GetOnlineFeaturesWithAggregatedSequence(userId interface{}, sequ
 		return nil, fmt.Errorf("feature entity name:%s not found", featureEntityName)
 	}
 
+	joinId := featureEntity.FeatureEntityJoinid
+
 	var wg sync.WaitGroup
 
 	featureViewMap := m.featureEntityJoinIdMap[featureEntity.FeatureEntityJoinid]
@@ -465,6 +467,104 @@ func (m *Model) GetOnlineFeaturesWithAggregatedSequence(userId interface{}, sequ
 			featuresResult[k] = v
 		}
 	}
+
+	if children := m.childEntitiesMap[joinId]; len(children) > 0 {
+		// read keys of child entities with deduplication
+		childJoinIdKeys := make(map[string][]interface{})
+		childJoinIdKeySet := make(map[string]map[string]struct{})
+		for _, row := range results {
+			if row == nil {
+				continue
+			}
+			for _, childJoinId := range children {
+				if val, exists := row[childJoinId]; exists && val != nil {
+					valStr := utils.ToString(val, "")
+					if valStr == "" {
+						continue
+					}
+					if childJoinIdKeySet[childJoinId] == nil {
+						childJoinIdKeySet[childJoinId] = make(map[string]struct{})
+					}
+					if _, seen := childJoinIdKeySet[childJoinId][valStr]; !seen {
+						childJoinIdKeySet[childJoinId][valStr] = struct{}{}
+						childJoinIdKeys[childJoinId] = append(childJoinIdKeys[childJoinId], val)
+					}
+				}
+			}
+		}
+
+		// read features of child entities
+		if len(childJoinIdKeys) > 0 {
+			featureViewCount := 0
+			for childJoinId := range childJoinIdKeys {
+				if featureViewMap, exists := m.featureEntityJoinIdMap[childJoinId]; exists {
+					featureViewCount += len(featureViewMap)
+				}
+			}
+
+			var mu sync.Mutex
+			joinIdFeaturesMap := make(map[string][]map[string]interface{})
+
+			var childWg sync.WaitGroup
+			for childJoinId, keys := range childJoinIdKeys {
+				if featureViewMap, exists := m.featureEntityJoinIdMap[childJoinId]; exists {
+					for _, featureView := range featureViewMap {
+						childWg.Add(1)
+						go func(featureView FeatureView, joinId string, keys []interface{}, featureViewCount int) {
+							defer childWg.Done()
+							features, err := featureView.getOnlineFeaturesWithCount(keys, m.featureNamesMap[featureView.GetName()], m.aliasNamesMap[featureView.GetName()], featureViewCount)
+							if err != nil {
+								fmt.Println(err)
+							}
+
+							mu.Lock()
+							joinIdFeaturesMap[joinId] = append(joinIdFeaturesMap[joinId], features...)
+							mu.Unlock()
+						}(featureView, childJoinId, keys, featureViewCount)
+					}
+				}
+			}
+			childWg.Wait()
+
+			// merge features of child entities
+			childFeatureIndex := make(map[string]map[string]map[string]interface{}) // join id : key : row values
+			for childJoinId, rows := range joinIdFeaturesMap {
+				keyToRow := make(map[string]map[string]interface{})
+				for _, row := range rows {
+					if val, ok := row[childJoinId]; ok {
+						valStr := utils.ToString(val, "")
+						if valStr == "" {
+							continue
+						}
+						if keyToRow[valStr] == nil {
+							keyToRow[valStr] = make(map[string]interface{})
+						}
+						for k, v := range row {
+							keyToRow[valStr][k] = v
+						}
+					}
+				}
+				childFeatureIndex[childJoinId] = keyToRow
+			}
+
+			// merge child entity features into final result
+			for _, childJoinId := range children {
+				if childIndex, ok := childFeatureIndex[childJoinId]; ok {
+					if rootChildVal, exists := featuresResult[childJoinId]; exists && rootChildVal != nil {
+						keyStr := utils.ToString(rootChildVal, "")
+						if keyStr != "" {
+							if childRow, found := childIndex[keyStr]; found {
+								for k, v := range childRow {
+									featuresResult[k] = v
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	return featuresResult, nil
 }
 
